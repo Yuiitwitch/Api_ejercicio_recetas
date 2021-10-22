e2060d2fc425e53ff84eb8bad592c2fde617e9ce
Alfonso Lara <86298325+Yuiitwitch@users.noreply.github.com>
2021-10-22 13:09:57 +0200

    Añadimos listening en el proyecto

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express= require('express');
 const colors = require('colors');
+const logger = require('.config/winston')
 
 const app=express();
 let PORT = process.env.YOUR_PORT || process.env.PORT || 3000;
 
 app.use(express.json());
+
+const router = require('./router.js');
+app.use(router);
+
+app.listen(PORT , () => {
+    console.log(`Server on port ${PORT}`.bgGreen.black);
+})
